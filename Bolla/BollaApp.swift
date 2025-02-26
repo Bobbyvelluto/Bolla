3008246f06dc1e0391de601b063a02f05f18f46b
Bobbyvelluto <Bobbyvelluto@icloud.com>
2025-02-26 12:35:03 +0100

    Initial Commit

---
+++ Bolla/BollaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BollaApp.swift
+//  Bolla
+//
+//  Created by Maurizio Arena on 26/02/25.
+//
+
+import SwiftUI
+
+@main
+struct BollaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BollaTests/BollaTests.swift
@@ -0,0 +1,17 @@
+//
+//  BollaTests.swift
+//  BollaTests
+//
+//  Created by Maurizio Arena on 26/02/25.
+//
+
+import Testing
+@testable import Bolla
+
+struct BollaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
